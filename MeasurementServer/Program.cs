ea71412f32d163400fc14230bb0432488b096cd6
Valdemartt <36989028+Valdemartt@users.noreply.github.com>
2019-03-13 08:59:49 +0100

    Add project files.

---
+++ MeasurementServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeasurementServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
