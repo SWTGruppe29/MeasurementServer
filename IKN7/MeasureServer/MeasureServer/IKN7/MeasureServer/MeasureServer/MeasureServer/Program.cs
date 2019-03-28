3b64f8d7d26cc121027ab237dd132382eddf7acd
Valdemar Tang <valdemar-tang@hotmail.com>
2019-03-28 11:16:48 +0100

    Added new Not CORE project

---
+++ IKN7/MeasureServer/MeasureServer/IKN7/MeasureServer/MeasureServer/MeasureServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace MeasurementServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UDPSocket s = new UDPSocket();
+            s.Server("10.0.0.1", 9000);
+            while (true)
+            {
+                s.Receive();
+            }
+        }
+    }
+}
